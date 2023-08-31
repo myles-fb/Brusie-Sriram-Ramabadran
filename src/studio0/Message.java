a7362853c368ca6859d41c95484e0327d7bf4813
chris <chris@10.232.165.158>
2023-08-31 12:05:12 -0500

    finished up guys ok

--- src/studio0/Message.java
+++ src/studio0/Message.java
@@ -4,5 +4,7 @@ public class Message {
 
 	public static void main(String args[]) {
 		System.out.println("Say hi to your group mates!");
+		System.out.println("I love Prof. SHook and my TA miles");
 	}
 }
+//what's up
